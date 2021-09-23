f612069f8d2da97a11fe7ef622b1e8c366d7f144
Rabee Mghrabi <rabeemghrabi93@gmail.com>
2021-09-23 16:26:17 +0100

    fetched weather description from API

--- app.js
+++ app.js
@@ -7,10 +7,16 @@ const app = express();
 
 app.get("/", function(req,res) {
 
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=a2c9a5fb7e2a6b62bc8c80f26ed990f4&units=metric'
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=Paris&units=metric&appid=a2c9a5fb7e2a6b62bc8c80f26ed990f4'
 
   https.get(url, function(response){
-    console.log(response);
+
+    response.on("data", function(data){
+      const parsedData = JSON.parse(data)
+      const weatherDescription = parsedData.weather[0].description
+      console.log(weatherDescription)
+    })
+
 
   })
 res.send('server up and running')
